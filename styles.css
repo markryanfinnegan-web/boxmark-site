2badeb7c84e92687cb499415affb7ae318ecf378
markryanfinnegan-web <markryanfinnegan@gmail.com>
2026-07-12 00:25:39 +0100

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+if (toggle && nav) toggle.addEventListener('click', () => nav.classList.toggle('open'));
+
+document.querySelectorAll('form').forEach(form => {
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    alert('Newsletter sign-up is ready to connect to Mailchimp, Brevo or another email service.');
+  });
+});
